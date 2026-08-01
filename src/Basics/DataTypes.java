32a01defaab9e43a27fa70456244a618600f2f1d
SamarpanKarki <samarpankarki824@gmail.com>
2026-08-01 22:36:30 +0545

    Practice Java data types

---
+++ src/Basics/DataTypes.java
@@ -0,0 +1,18 @@
+package Basics;
+
+public class DataTypes {
+    public static void main(String[] args) {
+
+        int age = 20;
+        double height = 5.9;
+        char grade = 'A';
+        boolean passed = true;
+        String name = "Samarpan";
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(grade);
+        System.out.println(passed);
+    }
+}
